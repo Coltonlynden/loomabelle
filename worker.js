7e9fdddeb530d784fc7ba7fd3766558a9e95debe
Coltonlynden <Colton.Lynden.Chambers@gmail.com>
2025-08-22 05:20:02 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,5 @@
-// Reserved for heavy ops (vector fill, PES/DST writers).
-// Hook: postMessage({type:'write-pes', stitches, bbox, px_per_mm})
-self.onmessage = (e)=>{
+// Reserved for heavy ops (PES/DST writers). Runs client-side only.
+self.onmessage = e => {
   const {type} = e.data || {};
-  if(type==='ping'){ postMessage({ok:true}); }
-  // PES/DST encoders will be added here in the next increment.
+  if(type==='ping') postMessage({ok:true});
 };

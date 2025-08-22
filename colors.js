2cbebbd20d9fe4d9d675dafadf192e55cb1762f2
Coltonlynden <Colton.Lynden.Chambers@gmail.com>
2025-08-22 05:18:58 -0500

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,4 +1,4 @@
-// Minimal Brother-like palette (RGB). Expandable.
+// Minimal Brother-like palette (RGB)
 window.EAS_THREADS = [
   {name:"White", rgb:[255,255,255]},
   {name:"Black", rgb:[0,0,0]},
@@ -14,13 +14,12 @@ window.EAS_THREADS = [
   {name:"Blue", rgb:[50,100,210]},
   {name:"Purple", rgb:[130,100,180]},
   {name:"Brown", rgb:[120,80,60]},
-  {name:"Grey", rgb:[150,150,150]}
+  {name:"Grey", rgb:[150,150,150]},
 ];
-
 window.nearestThread = function(rgb){
   let best=0,bd=1e9;
   for(let i=0;i<EAS_THREADS.length;i++){
-    const t=EAS_THREADS[i], d = (t.rgb[0]-rgb[0])**2+(t.rgb[1]-rgb[1])**2+(t.rgb[2]-rgb[2])**2;
+    const t=EAS_THREADS[i], d=(t.rgb[0]-rgb[0])**2+(t.rgb[1]-rgb[1])**2+(t.rgb[2]-rgb[2])**2;
     if(d<bd){bd=d;best=i;}
   }
   return {index:best, ...EAS_THREADS[best]};

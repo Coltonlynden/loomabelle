4bbd4554e2e05494678468038427c89aa4773fc9
Coltonlynden <Colton.Lynden.Chambers@gmail.com>
2025-08-25 19:35:05 -0500

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,26 +1,26 @@
-// Minimal Brother-like palette (RGB)
-window.EAS_THREADS = [
-  {name:"White", rgb:[255,255,255]},
-  {name:"Black", rgb:[0,0,0]},
-  {name:"Rose Pink", rgb:[236,170,170]},
-  {name:"Pink", rgb:[230,150,170]},
-  {name:"Salmon", rgb:[230,140,120]},
-  {name:"Red", rgb:[200,40,40]},
-  {name:"Orange", rgb:[230,150,80]},
-  {name:"Yellow", rgb:[245,220,100]},
-  {name:"Lime", rgb:[160,210,90]},
-  {name:"Green", rgb:[50,140,80]},
-  {name:"Sky", rgb:[120,190,230]},
-  {name:"Blue", rgb:[50,100,210]},
-  {name:"Purple", rgb:[130,100,180]},
-  {name:"Brown", rgb:[120,80,60]},
-  {name:"Grey", rgb:[150,150,150]},
-];
-window.nearestThread = function(rgb){
-  let best=0,bd=1e9;
-  for(let i=0;i<EAS_THREADS.length;i++){
-    const t=EAS_THREADS[i], d=(t.rgb[0]-rgb[0])**2+(t.rgb[1]-rgb[1])**2+(t.rgb[2]-rgb[2])**2;
-    if(d<bd){bd=d;best=i;}
+// small helper to get a quick palette from an image (k-means lite)
+window.EAS = window.EAS || {};
+EAS.colors = {
+  quantize(imgData, k=6){
+    const {data,w,h} = imgData, N=w*h, S=4;
+    let pts=[]; pts.length=0;
+    for(let i=0;i<N;i+=Math.max(1,Math.floor(N/4000))){
+      const j=i*4; pts.push([data[j],data[j+1],data[j+2]]);
+    }
+    // init centers
+    const C=pts.slice(0,k);
+    for(let iter=0;iter<8;iter++){
+      const sum=Array.from({length:k},()=>[0,0,0,0]);
+      for(const p of pts){
+        let bi=0,bd=1e9;
+        for(let c=0;c<k;c++){
+          const d=(p[0]-C[c][0])**2+(p[1]-C[c][1])**2+(p[2]-C[c][2])**2;
+          if(d<bd){bd=d;bi=c;}
+        }
+        const s=sum[bi]; s[0]+=p[0]; s[1]+=p[1]; s[2]+=p[2]; s[3]++;
+      }
+      for(let c=0;c<k;c++){ if(sum[c][3]) C[c]=[sum[c][0]/sum[c][3]|0,sum[c][1]/sum[c][3]|0,sum[c][2]/sum[c][3]|0]; }
+    }
+    return C;
   }
-  return {index:best, ...EAS_THREADS[best]};
 };
